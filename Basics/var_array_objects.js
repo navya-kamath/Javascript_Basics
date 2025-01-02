4e7d2ff9f3bc78bac1e7f10b5ac47057139fe47c
navya-kamath <maynavya@gmail.com>
2025-01-02 11:48:48 +0530

    Re-added the Basics folder with JavaScript files

---
+++ Basics/var_array_objects.js
@@ -0,0 +1,24 @@
+// Variables
+// Rules 
+// 1. Name contains $,_,letter,digits
+// 2. Name must begin with letter or it can begin with $ or _
+// 3. Should be of camelCase.
+var name = 'Helllo';
+var num = 20;
+var isTrue = true;
+var fraction = 20.3;
+
+// Arrays
+var arr = ['Name', 24, true, 98.3];
+
+//rAccess element from Arrays
+var a = arr[3];
+
+// Replacing element in the Arrays
+arr[0]= 'FullName';
+console.log(arr);
+
+// Determine length of the array
+arr.length
+
+
